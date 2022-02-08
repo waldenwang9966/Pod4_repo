3b6004e6eee4882cb3c5be5b510a598740610009
Sierra Nash <sasianan19@gmail.com>
2022-02-07 18:39:14 -0700

    Create challenge_04 folder with two challenge files

---
+++ Sierra/challenge_04/temperature.py
@@ -0,0 +1,26 @@
+# 1. Convert a temperature of 100 degrees fahrenheit to celsius
+celsius_100 = (100 - 32) * 5/9
+print(celsius_100)
+# Declared var called "celsius_100" to convert from F to C using formula (F - 32) * 5/9. Result is a float because of the 5/9 division.
+
+
+# 2. Convert a temperature of 0 degrees fahrenheit to celsius
+celsius_0 = (0 - 32) * 5/9
+print(celsius_0)
+# Declared var called "celsius_0" to convert from F to C using same formula and result it a float again. 
+
+
+# 3. Convert a temperature of 34.2 degrees fahrenheit to celsius
+print((34.2 - 32) * 5/9)
+# Converting 34.2 degrees F to C using same equation, this time without declaring variables!
+
+
+# 4. Convert a temperature of 5 degrees celsius to fahrenheit?
+print((5 * 1.8) + 32)
+# Converting 5 degrees C to F using F * 1.8 plus 32. 
+
+# 5. What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
+celsius_temp = (30.2 * 1.8) + 32
+print(celsius_temp)
+print(f'30.2 degrees celsius is hotter than 85.1 degrees fahrenheit; 30.2 degrees celsius converts to 86.36 degrees F!')
+# Converted 30.2 degrees C to F to check if it is hotter than 85.1 degrees F

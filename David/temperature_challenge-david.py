a30add523d5affda49779de803144e75ac60a044
Dave218 <dvk5775@yahoo.com>
2022-02-09 14:21:14 -0600

    adding my temp challenge

---
+++ David/temperature_challenge-david.py
@@ -0,0 +1,22 @@
+# 1 converting f-c is a float
+celsius_100 = (100-32) * 5/9
+print(celsius_100)
+print(type(celsius_100))
+
+# 2 converting far to cel
+celsius_0 = (0-32) * 5/9
+print(celsius_0)
+
+# 3  converting far to cel without using variables
+print((34.2-32)*5/9)
+
+# 4 converting cel-far
+print((5*9/5)+32)
+
+# 5 32.2 cel is hotter than 85.1 faren
+print((30.2*9/5)+32)
+
+
+
+
+# temp = float(input("Enter temperature in Fahrenheit: "))

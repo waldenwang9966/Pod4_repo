d0c13cc33e7834442aa93b989acbb3d07245b48c
Jonathanlouiskirk <qcpskirk@gmail.com>
2022-02-09 16:38:29 -0500

    draft of first python challenge

---
+++ Jonathan/first_python_challenge.py
@@ -0,0 +1,10 @@
+meal = ('I had taco salad for lunch')
+print (meal)
+meal = ('I want steak, potatoes and corn on the cob for dinner')
+print (meal)
+print ('how old is Google?')
+Google_founded = (1993)
+Current_date = (2022)
+print (Current_date - Google_founded)
+
+

bed4fc94df58c6f4245be762d9815d75d9d31375
Jonathanlouiskirk <qcpskirk@gmail.com>
2022-02-10 15:23:07 -0500

    temerature_challenge.py

---
+++ Jonathan/temperature_challenge.py
@@ -0,0 +1,10 @@
+celsius_100 = 100 - 32 * 5/9 #assigned and saved value to variable
+print (celsius_100) #print command to convert from F to C
+#the answer to question number 12 is Float because there is a decimal point
+celsius_0 = 0 - 32 * 5/9
+print (celsius_0)
+print (34.2 - 32 * 5/9)#coverted this tempurature from F to C without assigning to a variable all in one print statement
+print (5 * 9/5 +32) # print statement,converted from C to F
+print (30.2 * 9/5 +32) # converted C to F comes to 86.36F so 
+#Answer to question number 22 is YES (30.2C) is hotter than (85.1F)
+

29c8d12c40242fcc38e3455cfefe613f94e8c12c
unknown <RogueStorm7>
2022-02-11 10:40:10 -0800

    made changes to file cleaned up comments and modified the paragraph to one line at Walden's suggestion

--- Mianta/05-challenges/nba_challenge_EXTRA.py
+++ Mianta/05-challenges/nba_challenge_EXTRA.py
@@ -27,10 +27,11 @@ jamal_m_percentage = 46 / 93
 fred_v_percentage = 43 / 110
 james_h_percentage = 37 / 109
 # 3.1: Printed out the paragraph but with only 1 sentence per line
-print('The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis.\n They sent a package of Brandon Ingram, Lonzo Ball, Josh Hart, and 3 first-round picks to New Orleans to land Davis.\n Those three have made good developments with the Pelicans, especially Brandon Ingram.\n But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an incredible season.\nLos Angeles has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA.\nThe Lakers ended the season atop the Western Conference with a record of 49-14.\nThey were narrowly behind the Bucks for the best record in the league.\n Davis proved to the final piece necessary for the Lakers to rebound from missing the playoffís last year. \n Los Angeles was a dominant club on both sides of the ball and are in a position to have another successful year next season.')
+lakers_info ='The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis. They sent a package of Brandon Ingram, Lonzo Ball, Josh Hart, and 3 first-round picks to New Orleans to land Davis. Those three have made good developments with the Pelicans, especially Brandon Ingram. But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an incredible season. Los Angeles has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA. The Lakers ended the season atop the Western Conference with a record of 49-14. They were narrowly behind the Bucks for the best record in the league. Davis proved to the final piece necessary for the Lakers to rebound from missing the playoffís last year. Los Angeles was a dominant club on both sides of the ball and are in a position to have another successful year next season.'
+print(lakers_info.replace('.', '/n'))
 
 # 3.2: Printed out the paragraph but with only 1 sentence per line
-lakers_info = 'The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis. \n They sent a package of Brandon Ingram, Lonzo Ball, Josh Hart, and 3 first-round picks to New Orleans to land Davis.\n Those three have made good developments with the Pelicans, especially Brandon Ingram. \n But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an incredible season.\n Los Angeles has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA.\n The Lakers ended the season atop the Western Conference with a record of 49-14.\n They were narrowly behind the Bucks for the best record in the league.\n Davis proved to the final piece necessary for the Lakers to rebound from missing the playoffís last year. \n Los Angeles was a dominant club on both sides of the ball and are in a position to have another successful year next season.'
+print(lakers_info.replace('.', '/n'))
 print(lakers_info.upper())
 
 # 3.3
@@ -46,12 +47,12 @@ print(int(lakers_are_best))
 print(float(lakers_are_best)) 
 
 #3.5: Type Conversion Part 2
-#Attempted to take each player's three point percentag and convert it to a string, printed them out.
-#but was unable to convert it because only intergers can be converted to strings
+#Attempted to take each player's 3-point percentage and convert it to a string, and print them out.
+#I  was unable to convert it because only intergers can be converted to strings
 print(str(jamal_m_percentage))
 print(str(fred_v_percentage))
 print(str(james_h_percentage))
-#took each player's three point percentag and convert it to a string, but noticed it was rounded doen to "0" there is no whole number to
+#took each player's 3-point percentage and converted it to a string, but noticed it was rounded down to "0" there is no whole number to
 # round up to. The larger numbers that could be rounded are after the decimal point.
 print(int(jamal_m_percentage))
 print(int(fred_v_percentage))

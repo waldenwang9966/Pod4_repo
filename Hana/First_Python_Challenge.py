5f379ca0dcceb5b4357d691016da50d4a21afed9
Hana Sanchez <ortizsanchezhana@outlook.com>
2022-02-14 19:20:41 -0500

    adding First_Python_Challenge.py

---
+++ Hana/First_Python_Challenge.py
@@ -0,0 +1,38 @@
+#1.1 What did I eat for lunch?
+meal = 'pizza and a drink'
+#1.2 print meal
+print(meal)
+
+#1.3 What do you want for dinner? 
+dinner = ' I want lasagna, salad and garlic bread'
+print(dinner)
+
+#2 How old is Google?
+#2.1: Google was founded in 1993. The current year is 2022. Create a variable called google_age, and use subraction
+# to figure out how old Google is
+google_age = 2022-1998
+print(google_age) #24
+
+#2.2: Print out a sentence about Google's age. Make sure to include your variable in the f-string!
+print(f'Google might be {google_age} years old but is still 10 years younger than me.')
+
+# 2.3 How many _months_ old is Google? 
+# Create a new variable google_age_months, and use multiplication to figure it out,
+# then print the info.
+# (Assume 12 months for each year, you don't need to check which month they started)
+
+google_age_months = google_age * 12 
+print(f'Google is {google_age_months}')
+
+
+#3.1 
+print(f'The line of code below is commented out because it produces many Syntax Errors.')
+print(f'Fix the problem and turn the comment back into regular Python code.')
+#completion_message = 'Completed the first Python challenge!
+
+# 3.2 What were the syntax errors that you fixed? print out a quick explanation of each one.
+print(f'I fixed the grammatical errors and added the proper ()')
+#3.3
+print(f'Turn the comment below back into regular Python code')
+print('I completed the first Python Challenge!!!')
+

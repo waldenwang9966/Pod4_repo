438ca541813feae3447a7ec4c57deff76fc76b89
waldenwang9966 <waldenwang4@gmail.com>
2022-02-24 19:33:17 -0500

    updated

--- week_4/importing/playlist_challenge.py
+++ week_4/importing/playlist_challenge.py
@@ -4,8 +4,6 @@
 # But, you'll run your functions here
 
 # 1 Import all the functions in playlist_functions.py
-import numpy as np
-from playlist_functions import *
 
 # This code initializes your playlist as an empty list. no songs in it yet!
 my_playlist = []
@@ -14,7 +12,6 @@ my_playlist = []
 # 2 Check what is in your playlist using the display_playlist() function
 # HINT: the display_playlist() function in playlist_functions.py to figure out how to use it
 print("Question 2")
-display_playlist(my_playlist)
 
 # 3 Add a song to my_playlist using the add_song() function
 # The song that you add should be a dictionary, with the following key-value pairs
@@ -24,24 +21,18 @@ display_playlist(my_playlist)
 """
 example_song = {'artist': 'Lauryn Hill', 'title': 'Everything Is Everything'}
 """
-add_song(my_playlist, {"artist": "Lauryn Hill", "title": "Everything Is Everything"})
 
 # 4 Check that you've added the song by running the display_playlist() function again
 print("Question 4")
-display_playlist(my_playlist)
 
 # 5 Add 2 more songs to my_playlist, then display it again using the display_playlist() function
 print("Question 5")
-add_song(my_playlist, {"artist": "Justin Bieber", "title": "Baby"})
-add_song(my_playlist, {"artist": "Cardi B", "title": "Bodak Yellow"})
-display_playlist(my_playlist)
 
 
 # 6 In playlist_functions.py, define a function called get_playlist_length()
 # See playlist_functions.py for details on how to define this function
 # THEN, call that function in this script to get the length of my_playlist
 print("Question 6")
-print(get_playlist_length(my_playlist))
 
 # 7 At the top of this script, import numpy using the usual alias
 
@@ -49,8 +40,6 @@ print(get_playlist_length(my_playlist))
 # TODO: using the mean() function from numpy, calculate the average of monthly_plays
 # You don't have to write any functions for this question
 print("Question 8")
-monthly_plays = [127030, 274920, 232453, 98278, 500301, 235462]
-print(np.mean(monthly_plays))
 
 
 # BONUS In playlist_functions.py, define a new function called play_track()
